0ac65bdef066c7bce6eb8ce52b75c87388959848
Patrick Stockwell <stockwellpatrick@gmail.com>
2019-01-23 12:08:04 +1100

    add initial routes

--- src/App.js
+++ src/App.js
@@ -1,28 +1,21 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+import Workouts from './tabs/Workouts'
+import Navigation from './components/Navigation'
+
+const About = () => <h2>About</h2>;
+const Users = () => <h2>Users</h2>;
+
+const App = () => (
+  <Router>
+    <div>
+      <Route path="/" exact component={Workouts} />
+      <Route path="/about/" component={About} />
+      <Route path="/users/" component={Users} />
+      <Navigation />
+    </div>
+  </Router>
+);
 
 export default App;
