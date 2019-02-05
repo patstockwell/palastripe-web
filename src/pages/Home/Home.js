44192d858bc73bd3e881fa06582b376822c19391
Patrick Stockwell <stockwellpatrick@gmail.com>
2019-02-05 22:45:55 +1100

    Use redux activeworkout in home component

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -6,7 +6,7 @@ import styled from 'styled-components';
 import Navigation from '../../components/Navigation';
 import PageHeading from '../../components/PageHeading';
 import Workout from '../../components/Workout';
-import { monday } from '../../helpers/data';
+import { workoutPropType, exercisePropType } from '../../helpers/data';
 
 const BottomScreenSpace = styled.div`
   height: 100px;
@@ -16,7 +16,7 @@ const StyledLink = styled(Link)`
   text-decoration: none;
 `;
 
-const Home = ({ workoutHistory }) => {
+const Home = ({ activeWorkout, workoutHistory }) => {
 
   const workouts = workoutHistory.map((workout, i) =>
     <Workout key={i} workoutRoutine={workout} />
@@ -26,7 +26,7 @@ const Home = ({ workoutHistory }) => {
     <div>
       <PageHeading>Home</PageHeading>
       <StyledLink to="/home/active-workout">
-        <Workout workoutRoutine={monday} />
+        <Workout workoutRoutine={activeWorkout} />
       </StyledLink>
       {workouts}
       <BottomScreenSpace />
@@ -38,16 +38,14 @@ const Home = ({ workoutHistory }) => {
 Home.propTypes = {
   workoutHistory: PropTypes.arrayOf(PropTypes.shape({
     date: PropTypes.object,
-    data: PropTypes.arrayOf(PropTypes.shape({
-      name: PropTypes.string,
-      weightInKilos: PropTypes.number,
-      sets: PropTypes.arrayOf(PropTypes.number),
-    })),
+    data: PropTypes.arrayOf(PropTypes.shape(exercisePropType)),
   })),
+  activeWorkout: PropTypes.shape(workoutPropType),
 };
 
 const mapStateToProps = state => ({
   workoutHistory: state.history,
+  activeWorkout: state.activeWorkout,
 });
 
 export default connect(mapStateToProps)(Home);
