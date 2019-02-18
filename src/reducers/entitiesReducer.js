e169db6d88d347d0784a40c1f43e205180ae3750
Patrick Stockwell <mail@deciphr.net>
2019-02-18 13:01:14 +1100

    Update weight on end of workout

--- src/reducers/entitiesReducer.js
+++ src/reducers/entitiesReducer.js
@@ -1,8 +1,37 @@
+import { END_WORKOUT } from '../helpers/constants';
+
 const entitiesReducer = (state, action) => {
   switch (action.type) {
+  case END_WORKOUT:
+    return endWorkout(state, action);
   default:
     return state;
   }
+};
+
+const endWorkout = (state, action) => {
+  const { activeWorkout: { order, exercises } } = action.payload;
+  const { exercises: allExercises } = state;
+
+  const updatedExercises = order
+    .reduce((acc, currId) => ({
+      ...acc,
+      [currId]: {
+        ...allExercises.byId[currId],
+        weightInKilos: exercises[currId].weightInKilos,
+      },
+    }), {});
+
+  return {
+    ...state,
+    exercises: {
+      ...allExercises,
+      byId: {
+        ...allExercises.byId,
+        ...updatedExercises,
+      },
+    },
+  };
 };
 
 export default entitiesReducer;
