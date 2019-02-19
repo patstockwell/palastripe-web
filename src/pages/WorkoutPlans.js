8825c5d5dcbc99af8746955b1c2b33b00bb24833
Patrick Stockwell <mail@deciphr.net>
2019-02-19 13:01:03 +1100

    map data to a plan object

--- src/pages/WorkoutPlans.js
+++ src/pages/WorkoutPlans.js
@@ -1,20 +1,52 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import LayoutTile from '../components/LayoutTile';
+import WorkoutTile from '../components/WorkoutTile';
 import BackSplash from '../components/BackSplash';
 import Navigation from '../components/Navigation';
 import Banner from '../components/Banner';
 import { purple, blue } from '../helpers/constants';
 
-const WorkoutPlans = ({ location }) => (
-  <BackSplash topLeft={purple} bottomRight={blue}>
-    <Banner />
-    <Navigation pathname={location.pathname}/>
-  </BackSplash>
-);
+const OpaqueTile = styled(LayoutTile)`
+  background-color: rgba(0, 0, 0, 0.3);
+  color: white;
+`;
+
+const WorkoutPlans = ({ entities, location }) => {
+  const {
+    workoutPlans: { byId: byPlan, allIds: allPlans },
+    workouts: { byId: byWorkout },
+    exercises: { byId: byExercise },
+  } = entities;
+
+  console.log(entities);
+  const singleWorkoutPlan = allPlans[0];
+  const dataForWorkouts = byPlan[singleWorkoutPlan].workouts
+    .map(id => byWorkout[id].exercises)
+    .map(exercises => exercises.map(id => byExercise[id]));
+  console.log(dataForWorkouts);
+
+  return (
+    <BackSplash topLeft={purple} bottomRight={blue}>
+      <Banner />
+      <OpaqueTile>
+        something here
+      </OpaqueTile>
+      <Navigation pathname={location.pathname}/>
+    </BackSplash>
+  );
+};
 
 WorkoutPlans.propTypes = {
   location: PropTypes.object,
+  entities: PropTypes.object,
 };
 
-export default WorkoutPlans;
+const mapStateToProps = state => ({
+  entities: state.entities,
+});
+
+export default connect(mapStateToProps)(WorkoutPlans);
 
