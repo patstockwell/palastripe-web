943908b6f4203c27981b99a79684309aa106774e
Patrick Stockwell <stockwellpatrick@gmail.com>
2019-05-15 22:22:40 +1000

    Animate arrow flip for show/hide

--- src/components/ActivityTile.tsx
+++ src/components/ActivityTile.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useSpring, animated } from 'react-spring';
 import styled from 'styled-components';
-import Line from '../assets/svg/Line';
+import DownArrow from '../assets/svg/DownArrow';
 import {
   isTimed,
   Activity, // eslint-disable-line no-unused-vars
@@ -46,9 +46,9 @@ const Duration = styled.div`
   padding: 0 8px;
 `;
 
-const SeeMoreLineWrapper = styled.div`
+const SeeMoreLineWrapper = styled(animated.div)`
   position: absolute;
-  bottom: -16px;
+  bottom: 0px;
   left: 50%;
   transform: translateX(-50%);
 `;
@@ -75,7 +75,9 @@ const ActivityTile:React.FC<Props> = ({
   selected,
 }) => {
   const animatedStyles = useSpring({
-    to: { height: show ? 300 : 0 },
+    height: show ? 300 : 0,
+    opacity: show ? 1 : 0,
+    x: show ? -180 : 0,
     config: { tension: 410, friction: 40 },
   });
 
@@ -96,12 +98,19 @@ const ActivityTile:React.FC<Props> = ({
           <p>{duration}</p>
         </Duration>
       </VisibleArea>
-      <animated.div style={animatedStyles}>
+      <animated.div style={{
+        height: animatedStyles.height,
+        opacity: animatedStyles.opacity,
+      }}>
         Hidden area!
       </animated.div>
       {selected &&
-        <SeeMoreLineWrapper>
-          <Line style={{ fill: 'grey' }}/>
+        <SeeMoreLineWrapper
+          style={{
+            transform: animatedStyles.x.interpolate(x =>
+              `translateX(-50%) rotate(${x}deg`)
+          }}>
+          <DownArrow style={{ fill: 'grey' }}/>
         </SeeMoreLineWrapper>
       }
     </Tile>
