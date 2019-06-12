53839fa42d970e32ee29260676f71df3bb589367
Patrick Stockwell <stockwellpatrick@gmail.com>
2019-06-12 21:40:15 +1000

    Add sliceWord function to highlight partial match

--- src/pages/EditWorkout.tsx
+++ src/pages/EditWorkout.tsx
@@ -9,6 +9,14 @@ interface OwnProps {
   animationStyles: any;
 }
 
+export const sliceWord = (word: string, index: number, length: number): {
+  start: string, highlight: string, end: string,
+} => ({
+  start: word.slice(0, index),
+  highlight: word.slice(index, index + length),
+  end: word.slice(index + length),
+});
+
 type Props = OwnProps & StateProps;
 
 const EditWorkout: React.FC<Props> = ({
@@ -22,11 +30,23 @@ const EditWorkout: React.FC<Props> = ({
     setInputValue(e.target.value);
   };
 
-  const matches = inputValue.length >= 3 &&
-    exercises.allIds.map(id => {
-      const { name } = exercises.byId[id];
-      if (name.toLowerCase().indexOf(inputValue) !== -1) {
-        return <div key={name}>{name}</div>;
+  const matches: JSX.Element[] = inputValue.length >= 3 &&
+    exercises.allIds.map((id: string) => {
+      const exercise = exercises.byId[id];
+      const name = exercise ? exercise.name : '';
+      const matchingIndex = name
+        .toLowerCase()
+        .indexOf(inputValue.toLowerCase());
+
+      if (matchingIndex !== -1) { // if we find a match
+        const { start, highlight, end, } =
+          sliceWord(name, matchingIndex, inputValue.length);
+
+        return (
+          <div key={name}>
+            {start}<strong>{highlight}</strong>{end}
+          </div>
+        );
       }
     });
 
