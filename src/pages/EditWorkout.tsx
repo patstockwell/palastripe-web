b57a2b58a0128de78e0ba20778aefc047074d942
Patrick Stockwell <stockwellpatrick@gmail.com>
2019-06-20 21:14:36 +1000

    Refactor search to use map + reduce

--- src/pages/EditWorkout.tsx
+++ src/pages/EditWorkout.tsx
@@ -3,19 +3,40 @@ import { connect } from 'react-redux';
 import { AnimatedSlidingPage } from './ActiveWorkout';
 import {
   Exercises, // eslint-disable-line no-unused-vars
+  Exercise, // eslint-disable-line no-unused-vars
 } from '../helpers/types';
 
 interface OwnProps {
   animationStyles: any;
 }
 
-export const sliceWord = (word: string, index: number, length: number): {
-  start: string, highlight: string, end: string,
-} => ({
+interface WordSlices {
+  start: string;
+  highlight: string;
+  end: string;
+}
+
+export const sliceWord = (
+  word: string, index: number, length: number
+): WordSlices => ({
   start: word.slice(0, index),
   highlight: word.slice(index, index + length),
   end: word.slice(index + length),
 });
+
+
+export const accumulateMatches =
+  (searchTerm: string, list: WordSlices[], e: Exercise) => {
+    const name = e ? e.name : '';
+    const matchingIndex = name
+      .toLowerCase()
+      .indexOf(searchTerm.toLowerCase());
+
+    return matchingIndex === -1 ? list : [
+      ...list,
+      sliceWord(name, matchingIndex, searchTerm.length),
+    ];
+  };
 
 type Props = OwnProps & StateProps;
 
@@ -30,25 +51,19 @@ const EditWorkout: React.FC<Props> = ({
     setInputValue(e.target.value);
   };
 
+  // Can I match more than one word?
+  // A search for `cur bicep` should match `Bicep Curl`
   const matches: JSX.Element[] = inputValue.length >= 3 &&
-    exercises.allIds.map((id: string) => {
-      const exercise = exercises.byId[id];
-      const name = exercise ? exercise.name : '';
-      const matchingIndex = name
-        .toLowerCase()
-        .indexOf(inputValue.toLowerCase());
-
-      if (matchingIndex !== -1) { // if we find a match
-        const { start, highlight, end, } =
-          sliceWord(name, matchingIndex, inputValue.length);
-
-        return (
-          <div key={name}>
-            {start}<strong>{highlight}</strong>{end}
-          </div>
-        );
-      }
-    });
+    exercises.allIds
+      .map((id: string): Exercise => exercises.byId[id])
+      .reduce((acc: WordSlices[], curr: Exercise): WordSlices[] => (
+        accumulateMatches(inputValue, acc, curr)
+      ), [])
+      .map(({ start, highlight, end }: WordSlices) => (
+        <div key={start + highlight + end}>
+          {start}<strong>{highlight}</strong>{end}
+        </div>
+      ));
 
   return (
     <AnimatedSlidingPage style={{ position, left }}>
