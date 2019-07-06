223c1060c2b8fdba68e8f015119c98d7ea439dbf
Patrick Stockwell <stockwellpatrick@gmail.com>
2019-07-06 17:52:50 +1000

    Add Tile style and dots for options menu

--- src/components/ActivityHistoryTile.tsx
+++ src/components/ActivityHistoryTile.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
+
+import Dots from '../assets/svg/Dots';
 import {
   getDiffInMinutes,
   formatDate,
@@ -9,23 +11,41 @@ import {
   Workout, // eslint-disable-line no-unused-vars
 } from '../helpers/types';
 import { opaqueImageInAfter } from './SharedStyles';
+import { gutterWidth } from '../helpers/constants';
 
 const Hr = styled.hr`
   border: none;
-  height: 1px;
+  height: 12px;
   background-color: lightgrey;
+  margin: 0;
 `;
 
 const Image = styled.div<{ image: string }>`
   width: 100%;
   height: 300px;
-  // background-color: black;
+  background-color: black;
   position: relative;
   z-index: -2;
 
   &::after {
     ${opaqueImageInAfter}
+    opacity: 0.9; // override the opacity set in opaqueImageInAfter
   }
+`;
+
+const TileHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Name = styled.p`
+  padding: ${gutterWidth}px;
+`;
+
+const OptionsButton = styled.button`
+  border: none;
+  background: none;
+  display: flex;
 `;
 
 interface Props {
@@ -41,8 +61,13 @@ const ActivityHistoryTile: React.FC<Props> = ({ workout }) => {
 
   return (
     <div>
+      <TileHeader>
+        <Name>{name}</Name>
+        <OptionsButton>
+          <Dots />
+        </OptionsButton>
+      </TileHeader>
       <Image image={workout.imageUrl} />
-      <p>{name}</p>
       <p>{`${day}, ${date} ${month}`}</p>
       <p>{totalTime}</p>
       <Hr />
