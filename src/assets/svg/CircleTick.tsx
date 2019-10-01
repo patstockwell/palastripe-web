dc3caf8a8e9e55cedffe9c69da19997c063f7f4a
Patrick Stockwell <stockwellpatrick@gmail.com>
2019-10-01 23:12:51 +1000

    Make svg circle animation optional

--- src/assets/svg/CircleTick.tsx
+++ src/assets/svg/CircleTick.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styled, { keyframes } from 'styled-components';
+import styled, { css, keyframes } from 'styled-components';
 import { green } from '../../helpers/constants';
 
 const stroke = keyframes `
@@ -23,17 +23,19 @@ const fill = keyframes `
   }
 `;
 
-const Circle = styled.circle`
+const Circle = styled.circle<{ animate: boolean }>`
   stroke-dasharray: 166;
   stroke-dashoffset: 166;
   stroke-width: 9;
   stroke-miterlimit: 10;
   stroke: ${green};
   fill: none;
+  ${({ animate }) => animate ? css`
   animation: ${stroke} 0.3s cubic-bezier(0.65, 0, 0.45, 1) forwards;
+  ` : 'stroke-dashoffset: 0;'}
 `;
 
-const Svg = styled.svg`
+const Svg = styled.svg<{ animate: boolean }>`
   width: 100%;
   border-radius: 50%;
   display: block;
@@ -41,29 +43,38 @@ const Svg = styled.svg`
   stroke: #fff;
   stroke-miterlimit: 10;
   box-shadow: inset 0px 0px 0px ${green};
+  ${({ animate }) => animate ? css`
   animation:
     ${fill} .1s ease-in-out .3s forwards,
     ${scale} .2s ease-in-out .4s both;
+  ` : `box-shadow: inset 0px 0px 0px 30px ${green};`}
 `;
 
-const Path = styled.path`
+const Path = styled.path<{ animate: boolean }>`
   transform-origin: 50% 50%;
   stroke-dasharray: 48;
   stroke-dashoffset: 48;
   stroke-width: 4;
+  ${({ animate }) => animate ? css`
   animation: ${stroke} 0.4s cubic-bezier(0.65, 0, 0.45, 1) 0.5s forwards;
+  ` : 'stroke-dashoffset: 0;'}
 `;
 
 interface Props {
   onAnimationEnd: () => void;
+  animate?: boolean;
 }
 
-const CircleTick: React.FC<Props> = ({ onAnimationEnd }) => (
+const CircleTick: React.FC<Props> = ({ animate = true, onAnimationEnd }) => (
   <Svg
     className="checkmark"
     viewBox="0 0 52 52"
+    animate={animate}
   >
-    <Circle className="checkmark__circle" cx="26" cy="26" r="25" fill="none"/>
+    <Circle
+      className="checkmark__circle" cx="26" cy="26" r="25" fill="none"
+      animate={animate}
+    />
     {/*
       Putting an animation listener on the svg results in 4 calls to
       `onAnimationEnd`. There are 2 animations on svg, 1 on Circle,
@@ -74,6 +85,7 @@ const CircleTick: React.FC<Props> = ({ onAnimationEnd }) => (
       className="checkmark__check"
       fill="none"
       d="M14.1 27.2l7.1 7.2 16.7-16.8"
+      animate={animate}
     />
   </Svg>
 );
