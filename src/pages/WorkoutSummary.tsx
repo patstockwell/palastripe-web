2e93c6e2e4cbf508a331401bdc736e4bb8b5eb5e
Patrick Stockwell <stockwellpatrick@gmail.com>
2019-12-17 19:53:23 +1100

    Update styles for workout summary splash page

--- src/pages/WorkoutSummary.tsx
+++ src/pages/WorkoutSummary.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import { connect } from 'react-redux';
 import { State, Workout } from '../helpers/types';
-import BackLinkBanner from '../components/BackLinkBanner';
+import Banner from '../components/Banner';
 import GoldCup from '../assets/svg/GoldCup';
 import { workoutTitleStyle, buttonStyle } from '../components/SharedStyles';
 import { bannerHeight } from '../helpers/constants';
@@ -11,6 +11,10 @@ import { Link } from 'react-router-dom';
 const Page = styled.div`
   text-align: center;
   padding: 24px 12px;
+
+  & svg {
+    margin: 12px 0;
+  }
 `;
 
 const Hr = styled.hr`
@@ -32,17 +36,27 @@ const Button = styled(Link)`
   max-width: 300px;
 `;
 
+const GreatJob = styled.p`
+  font-weight: 600;
+  margin: 12px 0;
+`;
+
+const Completed = styled.p`
+  color: grey;
+  margin: 12px 0;
+`;
+
 const WorkoutSummary: React.FC<StateProps> = ({ firstName, workout }) => {
   const { name: workoutName } = workout;
   console.log(firstName);
 
   return (
     <>
-      <BackLinkBanner heading="Workout Summary" />
+      <Banner heading="Workout Summary" />
       <Hr />
       <Page>
-        <p>Great Job{firstName && `, ${firstName}`}!</p>
-        <p>You just completed</p>
+        <GreatJob>Great Job{firstName && `, ${firstName}`}!</GreatJob>
+        <Completed>You just completed</Completed>
         <Title>{workoutName}</Title>
         <GoldCup height="80px" />
         <p>Workout done! Get some nutritious food and take a well earned rest.</p>
