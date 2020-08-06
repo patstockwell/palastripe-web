506355c2016fdb4529ebb7391ef50232431dcd99
Patrick Stockwell <mail@deciphr.net>
2020-08-06 19:48:26 +1000

    Reset the selected exercise when navigating to activity-search

--- src/pages/ActiveWorkout/CustomWorkoutTile.tsx
+++ src/pages/ActiveWorkout/CustomWorkoutTile.tsx
@@ -1,5 +1,5 @@
 import React  from 'react';
-import { Link } from 'react-router-dom';
+import {Link} from 'react-router-dom';
 import styled from 'styled-components';
 
 import {
@@ -8,8 +8,9 @@ import {
   lightGrey3,
   charcoal,
 } from '../../helpers/constants';
-import { customWorkoutId } from '../../workoutData/workouts/customWorkout';
-import { activitySearchPath } from './ActivitySearch';
+import {customWorkoutId} from '../../workoutData/workouts/customWorkout';
+import {activitySearchPath} from './ActivitySearch';
+import {useSelectedExercise} from '../../context/useSelectedExercise';
 
 const AddActivityButton = styled(Link)`
   height: ${tileMinHeight}px;
@@ -32,16 +33,27 @@ interface Props {
   showHiddenArea: boolean;
 }
 
-// This tile sits at the bottom of the custom workout and links to the activity-search
-export const CustomWorkoutTile: React.FC<Props> = ({ setShowHiddenArea }) => (
-  <AddActivityButton
-    // Always collapse the last active tile in the work when transitioning to
-    // the activity search. This ensures that when we finish the activity
-    // search and return to the active workout, the animation can start from a
-    // closed position and the scroll-to-top function will work correctly.
-    onClick={() => setShowHiddenArea(false)}
-    to={`/workouts/${customWorkoutId}/${activitySearchPath}`}
-  >
-    + Add a set
-  </AddActivityButton>
-);
+export const CustomWorkoutTile: React.FC<Props> = ({ setShowHiddenArea }) => {
+  const {setSelectedExercise} = useSelectedExercise();
+
+  return (
+    <AddActivityButton
+      onClick={() => {
+        // Always collapse the last active tile in the workout when
+        // transitioning to the activity search. This ensures that when we
+        // finish the activity search and return to the active workout, the
+        // animation can start from a closed position and the scroll-to-top
+        // function will work correctly.
+        setShowHiddenArea(false);
+        // Reset the selected exercise so that if the user cancels the search
+        // and returns to the active workout, then nothing will be selected and
+        // you will be at the top of the screen. This avoids the rest timer
+        // popping up and a tile being opened and pulled into view.
+        setSelectedExercise({ index: 0, groupId: '' });
+      }}
+      to={`/workouts/${customWorkoutId}/${activitySearchPath}`}
+    >
+      + Add a set
+    </AddActivityButton>
+  );
+};
