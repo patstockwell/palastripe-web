d6f4bd8c68fb0b71db4f00b92ca337eb886b2c50
Patrick Stockwell <mail@deciphr.net>
2020-08-14 11:08:21 +1000

    Improve repeat workout alerts for existing workout

--- src/pages/Activity/ActivityHistoryOptionsMenu.tsx
+++ src/pages/Activity/ActivityHistoryOptionsMenu.tsx
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+import {useHistory} from 'react-router-dom';
 
 import {ReduxAction, Activity, State} from '../../helpers/types';
 import {
@@ -28,10 +29,12 @@ export const ActivityHistoryOptionsMenu: React.FC<Props> = ({
   workout,
   historyLink,
 }) => {
+  const {activeWorkout} = useSelector((s: State) => s);
   const [showExistingWorkoutAlert, setShowExistingWorkoutAlert] = useState(false);
   const [showDeleteWorkoutAlert, setShowDeleteWorkoutAlert] = useState(false);
   const {setActiveWorkout} = useActiveWorkout();
   const {setActivityPageScrollPosition} = useScrollPosition();
+  const history = useHistory();
   const {
     name: activeWorkoutName,
     id: activeWorkoutId,
@@ -42,11 +45,24 @@ export const ActivityHistoryOptionsMenu: React.FC<Props> = ({
     setShowDeleteWorkoutAlert(false);
   };
 
-  const repeatWorkout = () => {
+  const handleRepeatWorkoutInitialClick = () => {
+    // If there is an existing workout, show an alert first
+    if (activeWorkout && activeWorkout.startTime) {
+      setShowExistingWorkoutAlert(true);
+      toggleMenu();
+    } else {
+      handleRepeatWorkoutConfirmation();
+      history.push(`/workouts/${workout.id}/`); // Else go straight there.
+    }
+  };
+
+  const handleRepeatWorkoutConfirmation = () => {
+    console.log('here', workout.id, workout.id === customWorkoutId);
     // Only set the active workout if it is the custom workout, otherwise,
     // clear the active workout first by setting it to null, and then let the
     // component render the workout as usual from the URL.
     if (workout.id === customWorkoutId) {
+      console.log('inside');
       const incompleteWorkout: Workout = {
         ...workout,
         startTime: (new Date).toISOString(),
@@ -60,9 +76,9 @@ export const ActivityHistoryOptionsMenu: React.FC<Props> = ({
       };
 
       setActiveWorkout(incompleteWorkout);
+    } else {
+      setActiveWorkout(null);
     }
-
-    setActiveWorkout(null);
   };
 
   return (
@@ -88,9 +104,7 @@ export const ActivityHistoryOptionsMenu: React.FC<Props> = ({
           Delete Workout
         </AlertButtonPurple>
 
-        <AlertButtonPurple
-          onClick={() => setShowExistingWorkoutAlert(true)}
-        >
+        <AlertButtonPurple onClick={handleRepeatWorkoutInitialClick} >
           Repeat Workout
         </AlertButtonPurple>
 
@@ -123,7 +137,7 @@ export const ActivityHistoryOptionsMenu: React.FC<Props> = ({
         messageText={`"${activeWorkoutName}" is not finished. Starting a new workout will clear all progress.`}
       >
         <AlertButtonPurple
-          onClick={repeatWorkout}
+          onClick={handleRepeatWorkoutConfirmation}
           to={`/workouts/${workout.id}/`}
         >
           Start new workout
