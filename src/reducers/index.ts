c7f8c0e73d3401459a2bf80307a65d62506d351f
Patrick Stockwell <mail@deciphr.net>
2022-02-26 22:03:22 +1100

    Remove unsued deps and upgrade eslint and redux

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,4 +1,4 @@
-import { combineReducers } from 'redux';
+import { combineReducers } from '@reduxjs/toolkit';
 import settings from './settingsReducer';
 import profile from './profileReducer';
 import activeWorkout from './activeWorkoutReducer';
